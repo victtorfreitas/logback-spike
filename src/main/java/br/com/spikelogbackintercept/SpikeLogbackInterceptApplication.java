23d47b147ca6acd006ace439be0f92da47e78bbc
victtor <victtofreitas95@gmail.com>
2022-08-21 01:35:22 -0300

    base project

---
+++ src/main/java/br/com/spikelogbackintercept/SpikeLogbackInterceptApplication.java
@@ -0,0 +1,13 @@
+package br.com.spikelogbackintercept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeLogbackInterceptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpikeLogbackInterceptApplication.class, args);
+  }
+
+}
