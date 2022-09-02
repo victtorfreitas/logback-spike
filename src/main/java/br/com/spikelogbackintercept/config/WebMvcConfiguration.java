1fa9a20980d2c77263b773f4aaaaa8ecf2bb01b7
victtor <victtorfreitas95@gmail.com>
2022-09-02 08:11:35 -0300

    Change import lib

--- src/main/java/br/com/spikelogbackintercept/config/WebMvcConfiguration.java
+++ src/main/java/br/com/spikelogbackintercept/config/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.spikelogbackintercept.config;
 
+import br.com.interceptorlib.MdcInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
